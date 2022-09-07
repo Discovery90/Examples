539d5cbb5564124969fc9969e1bc3d8ee0dddced
Discovery <strong.nino@mail.ru>
2022-09-07 21:07:50 +0300

    add Zadacha 13 Seminar2

---
+++ Example11_Seminar2 zadacha13/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 13: Напишите программу, которая выводит 
+// третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+Console.WriteLine("Введите любое трёхзначное число: ");
+int zavdv = Convert.ToInt32(Console.ReadLine());
+string zavdvText = Convert.ToString(zavdv);
+if (zavdvText.Length > 2)
+{
+    Console.WriteLine($"третья цыфра" + zavdvText[2]);
+}
+else
+{
+Console.WriteLine("третья цифра отсутствует ");
+}
+
+
